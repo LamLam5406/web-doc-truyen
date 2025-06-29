dd77c414e60a2635b28dd15874e959de09fec421
LamLam5406 <xuando272004@gmail.com>
2025-06-29 21:17:40 +0700

    🛠 Fix CORS for Vercel

--- back/server.js
+++ back/server.js
@@ -9,7 +9,8 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: ['http://localhost:5173', 'https://web-doc-truyen-six.vercel.app'],
+  credentials: true
 }));
 
 // Routes
